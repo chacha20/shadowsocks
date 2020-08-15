84d7433b763e625abda2cde8834fb52302d490bd
chacha20 <46554415+chacha20@users.noreply.github.com>
2020-08-15 23:16:02 +0800

    Update shadowsocks-all.sh

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -38,7 +38,7 @@ cur_dir=$( pwd )
 software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 
 libsodium_file="libsodium-1.0.18"
-libsodium_url="https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz"
+libsodium_url="https://download.libsodium.org/libsodium/releases/LATEST.tar.gz"
 
 mbedtls_file="mbedtls-2.13.0"
 mbedtls_url="https://tls.mbed.org/download/mbedtls-2.13.0-gpl.tgz"
